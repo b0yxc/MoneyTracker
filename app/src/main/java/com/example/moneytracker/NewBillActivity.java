545ad0b10a8bfe9bd3cf9b0176f9a8d98552d375
b0yxc <25azb1@gmail.com>
2019-05-23 18:28:59 +0200

    added first two activities

---
+++ app/src/main/java/com/example/moneytracker/NewBillActivity.java
@@ -0,0 +1,13 @@
+package com.example.moneytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_bill);
+    }
+}
